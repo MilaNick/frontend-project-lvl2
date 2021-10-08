ef6b44cc50b38990159f0a1ff8f33d66b2e866d1
Mila Zolotova <l.zolotova@yahoo.com>
2021-10-08 17:06:42 +0300

    step 1,key help output

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,2 +1,11 @@
 #!/usr/bin/env node
+import { program } from 'commander';
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .option('-v, --version', 'output the version number 1')
+  .helpOption('-h, --help', 'output usage information');
+
+program.parse();
 console.log('Привет')
