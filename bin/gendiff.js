163acf30cb49eba08193e7c442a15c88df7cd69e
Mila Zolotova <l.zolotova@yahoo.com>
2021-10-08 21:39:23 +0300

    2 step,added arguments, format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,9 +3,11 @@ import { program } from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1')
-  .option('-v, --version', 'output the version number 1')
-  .helpOption('-h, --help', 'output usage information');
+  .version('0.0.1','-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+
 
 program.parse();
 console.log('Привет')
