21318da05d69db260d3405ccd4863b353905b106
Mila Zolotova <l.zolotova@yahoo.com>
2021-11-03 00:42:30 +0300

    changed index.js

--- index.js
+++ index.js
@@ -3,17 +3,13 @@
 import fs from 'fs';
 import path from 'path';
 import parse from './src/parsers.js';
-
-const genDiff = (filepath1, filepath2) => {
+const getObject = (filepath) => {
   const pathToDataDir = '/home/solo/frontend-project-lvl2/__fixtures__';
-  const file1 = fs.readFileSync(path.resolve(pathToDataDir, filepath1), { encoding: 'utf8' });
-  const file2 = fs.readFileSync(path.resolve(pathToDataDir, filepath2), { encoding: 'utf8' });
-  const format1 = path.extname(filepath1).slice(1);
-  const format2 = path.extname(filepath1).slice(1);
-  const obj1 = parse(format1, file1);
-  const obj2 = parse(format2, file2);
-  const rows = [];
-  const newSymbol = Symbol('replacement');
+  const file = fs.readFileSync(path.resolve(pathToDataDir, filepath), { encoding: 'utf8' });
+  const format = path.extname(filepath).slice(1);
+  return parse(format, file);
+}
+const getDiff = (obj1, obj2, rows, newSymbol) => {
   const uniqKeys = Array.from(new Set([...Object.keys(obj1), ...Object.keys(obj2)])).sort();
   const joint = uniqKeys.reduce((acc, key) => {
     acc[key] = [(key in obj1) ? obj1[key] : newSymbol, (key in obj2) ? obj2[key] : newSymbol];
@@ -37,6 +33,13 @@ const genDiff = (filepath1, filepath2) => {
       rows.push(`+ ${key}: ${joint[key][1]}`);
     }
   });
+}
+const genDiff = (filepath1, filepath2) => {
+  const obj1 = getObject(filepath1);
+  const obj2 = getObject(filepath2);
+  const rows = [];
+  const newSymbol = Symbol('replacement');
+  getDiff(obj1, obj2, rows, newSymbol);
   return (`{\n${rows.map((str) => `  ${str}`).join('\n')}\n}`);
 };
 export default genDiff;
