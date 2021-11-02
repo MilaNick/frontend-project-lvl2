1bc89cbdf43762eeb4b29c6ae42b0d02adeaced0
Mila Zolotova <l.zolotova@yahoo.com>
2021-11-03 00:45:55 +0300

    step 6: changed index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-// import yaml from 'js-yaml';
-
 import fs from 'fs';
 import path from 'path';
 import parse from './src/parsers.js';
+
 const getObject = (filepath) => {
   const pathToDataDir = '/home/solo/frontend-project-lvl2/__fixtures__';
   const file = fs.readFileSync(path.resolve(pathToDataDir, filepath), { encoding: 'utf8' });
   const format = path.extname(filepath).slice(1);
   return parse(format, file);
-}
+};
 const getDiff = (obj1, obj2, rows, newSymbol) => {
   const uniqKeys = Array.from(new Set([...Object.keys(obj1), ...Object.keys(obj2)])).sort();
   const joint = uniqKeys.reduce((acc, key) => {
@@ -33,7 +32,7 @@ const getDiff = (obj1, obj2, rows, newSymbol) => {
       rows.push(`+ ${key}: ${joint[key][1]}`);
     }
   });
-}
+};
 const genDiff = (filepath1, filepath2) => {
   const obj1 = getObject(filepath1);
   const obj2 = getObject(filepath2);
